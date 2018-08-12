3399c050c48cd09f9e20b544ed4a36c6e907c598
堺俊之 <toshi6661024@gmail.com>
2018-08-12 21:31:23 +0900

    fix dome code

--- codes/train_xgb.py
+++ codes/train_xgb.py
@@ -2,20 +2,23 @@ import numpy as np
 import pandas as pd
 import gc
 import math
+import tqdm
 
-from sklearn import model_selection
+# from sklearn import model_selection
 from sklearn.model_selection import ParameterGrid, StratifiedKFold
-from sklearn.preprocessing import RobustScaler
+# from sklearn.preprocessing import RobustScaler
 import xgboost as xgb
 
 import warnings
 warnings.filterwarnings('ignore')
 
-#A function to calculate Root Mean Squared Logarithmic Error (RMSLE)
+
+# A function to calculate Root Mean Squared Logarithmic Error (RMSLE)
 def rmsle(y, y_pred):
-	assert len(y) == len(y_pred)
-	terms_to_sum = [(math.log(y_pred[i] + 1) - math.log(y[i] + 1)) ** 2.0 for i,pred in enumerate(y_pred)]
-	return (sum(terms_to_sum) * (1.0/len(y))) ** 0.5
+    assert len(y) == len(y_pred)
+    terms_to_sum = [(math.log(y_pred[i] + 1) - math.log(y[i] + 1)) ** 2.0 for i, pred in enumerate(y_pred)]
+    return (sum(terms_to_sum) * (1.0 / len(y))) ** 0.5
+
 
 train_df = pd.read_csv('../input/train.csv')
 test_df = pd.read_csv('../input/test.csv')
@@ -23,20 +26,20 @@ test_df = pd.read_csv('../input/test.csv')
 # check Null values
 print("Total Train Features with NaN Values = " + str(train_df.columns[train_df.isnull().sum() != 0].size))
 if (train_df.columns[train_df.isnull().sum() != 0].size):
-	print("Features with NaN => {}".format(list(train_df.columns[train_df.isnull().sum() != 0])))
-	train_df[train_df.columns[train_df.isnull().sum() != 0]].isnull().sum().sort_values(ascending = False)
+    print("Features with NaN => {}".format(list(train_df.columns[train_df.isnull().sum() != 0])))
+    train_df[train_df.columns[train_df.isnull().sum() != 0]].isnull().sum().sort_values(ascending=False)
 
 print("Total Test Features with NaN Values = " + str(test_df.columns[test_df.isnull().sum() != 0].size))
 if (test_df.columns[test_df.isnull().sum() != 0].size):
     print("Features with NaN => {}".format(list(test_df.columns[test_df.isnull().sum() != 0])))
-    test_df[test_df.columns[test_df.isnull().sum() != 0]].isnull().sum().sort_values(ascending = False)
+    test_df[test_df.columns[test_df.isnull().sum() != 0]].isnull().sum().sort_values(ascending=False)
 
 # check and remove constant columns
 colsToRemove = []
 for col in train_df.columns:
-	if col != 'ID' and col != 'target':
-		if train_df[col].std() == 0:
-			colsToRemove.append(col)
+    if col != 'ID' and col != 'target':
+        if train_df[col].std() == 0:
+            colsToRemove.append(col)
 
 # remove constant columns in the training set
 train_df.drop(colsToRemove, axis=1, inplace=True)
@@ -45,26 +48,28 @@ train_df.drop(colsToRemove, axis=1, inplace=True)
 test_df.drop(colsToRemove, axis=1, inplace=True)
 
 print("Removed '{}' Constant Columns\n".format(len(colsToRemove)))
-#print(colsToRemove)
+# print(colsToRemove)
+
 
 def duplicate_columns(frame):
-	groups = frame.columns.to_series().groupby(frame.dtypes).groups
-	dups = []
+    groups = frame.columns.to_series().groupby(frame.dtypes).groups
+    dups = []
 
-	for t, v, in groups.items():
+    for t, v, in groups.items():
 
-		cs = frame[v].columns
-		vs = frame[v]
-		lcs = len(cs)
+        cs = frame[v].columns
+        vs = frame[v]
+        lcs = len(cs)
 
-		for i in range(lcs):
-			ia = vs.iloc[:, i].values
-			for j in range(i+1, lcs):
-				ja = vs.iloc[:, j].values
-				if np.array_equal(ia, ja):
-					dups.append(cs[i])
-					break
-	return dups
+        for i in range(lcs):
+            ia = vs.iloc[:, i].values
+            for j in range(i + 1, lcs):
+                ja = vs.iloc[:, j].values
+                if np.array_equal(ia, ja):
+                    dups.append(cs[i])
+                    break
+    return dups
+
 
 colsToRemove = duplicate_columns(train_df)
 print(colsToRemove)
@@ -77,14 +82,16 @@ test_df.drop(colsToRemove, axis=1, inplace=True)
 
 print("Removed '{}' Duplicate Columns\n".format(len(colsToRemove)))
 
+
 # drop sparse data
 def drop_sparse(train, test):
-	flist = [x for x in train.columns if not x in ['ID', 'target']]
-	for f in flist:
-		if len(np.unique(train[f])) < 2:
-			train.drop(f, axis=1, inplace=True)
-			test.drop(f, axis=1, inplace=True)
-	return train, test
+    flist = [x for x in train.columns if not x in ['ID', 'target']]
+    for f in flist:
+        if len(np.unique(train[f])) < 2:
+            train.drop(f, axis=1, inplace=True)
+            test.drop(f, axis=1, inplace=True)
+    return train, test
+
 
 train_df, test_df = drop_sparse(train_df, test_df)
 
@@ -140,6 +147,7 @@ for params in tqdm(list(ParameterGrid(all_params))):
         break
 
     sc_rmsle = np.mean(list_rmsle_score)
+
     if min_score > sc_rmsle:
         print("min_score:{}".format(sc_rmsle))
         min_score = sc_rmsle
