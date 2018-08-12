30e6ca6e01675d03cab859f017ccf98feff61522
slt666666 <toshi6661024@gmail.com>
2018-08-12 05:38:36 +0000

    add XGB

--- codes/samrat.py
+++ codes/samrat.py
@@ -113,7 +113,7 @@ def run_lgb(train_X, train_y, val_X, val_y, test_X):
 		"learning_rate": 0.005,
 		"bagging_fraction": 0.6,
 		"feature_fraction": 0.6,
-		"bagging_frequency": 6,
+		"bagging_freq": 6,
 		"bagging_seed": 42,
 		"verbosity": -1,
 		"seed": 42
@@ -138,8 +138,35 @@ print("LightGBM Training Completed...")
 # feature importance
 print("Feature importance...")
 gain = model.feature_importance('gain')
-featureimp = pd.Dataframe({'feature':model.feature_name(),
+featureimp = pd.DataFrame({'feature':model.feature_name(),
 			'split':model.feature_importance('split'),
 			'gain':100 * gain / gain.sum()}).sort_values('gain', ascending=False)
 print(featureimp[:50])
+
+# XGB modeling
+def run_xgb(train_X, train_y, val_x, val_y, test_X):
+	params = {'objective': 'reg:linear',
+		'eval_metric': 'rmse',
+		'eta': 0.001,
+		'max_depth': 10,
+		'subsample': 0.6,
+		'colsample_bytree': 0.6,
+		'alpha': 0.001,
+		'random_state': 42,
+		'silent': True}
+
+	tr_data = xgb.DMatrix(train_X, train_y)
+	va_data = xgb.DMatrix(val_X, val_y)
+
+	watchlist = [(tr_data, 'train'), (va_data, 'valid')]
+
+	model_xgb = xgb.train(params, tr_data, 2000, watch_list, maximize=False, early_stopping_rounds = 100, verbose_eval=100)
+	dtest = xgb.DMatrix(test_X)
+	xgb_pred_y = np.expm1(model_xgb.prediction(dtest, ntree_limit=model_xgb.best_ntree_limit))
+
+	return xgb_pred_y, model_xgb
+
+# Training XGB
+pred_test_xgb, model_xgb = run_xgb(dev_X, dev_y, val_X, val_y, X_test)
+print("XGB Training Completed...")
 
