779f71f0c9abdf691445b7c192d38a17af02788c
堺俊之 <toshi6661024@gmail.com>
2018-08-13 11:33:01 +0900

    fix rmsle

--- codes/train_xgb.py
+++ codes/train_xgb.py
@@ -15,7 +15,7 @@ warnings.filterwarnings('ignore')
 
 # A function to calculate Root Mean Squared Logarithmic Error (RMSLE)
 def rmsle(y_pred, y):
-    assert len(y) == len(y_pred)
+    assert y.shape[0] == y_pred.shape[0]
     terms_to_sum = [(math.log(y_pred[i] + 1) - math.log(y[i] + 1)) ** 2.0 for i, pred in enumerate(y_pred)]
     return (sum(terms_to_sum) * (1.0 / len(y))) ** 0.5
 
