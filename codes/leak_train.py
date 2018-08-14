9de95088fbcb07b26be78e6154242160cc493b83
堺俊之 <toshi6661024@gmail.com>
2018-08-15 01:25:29 +0900

    dele plt

--- codes/leak_train.py
+++ codes/leak_train.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from tqdm import tqdm
 from xgboost import XGBRegressor
 from sklearn.metrics import mean_squared_error
