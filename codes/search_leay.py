65d4bf31f347b9e1ee14f32b706599037e06554d
堺俊之 <toshi6661024@gmail.com>
2018-08-15 16:58:15 +0900

    add features

--- codes/search_leay.py
+++ codes/search_leay.py
@@ -62,7 +62,6 @@ def compiled_leak_result():
             "% of correct leaks values in train ",
             leaky_value_corrects[-1]
         )
-        print(train_leak["compiled_leak"])
         tmp = train_leak.copy()
         tmp.loc[zeroleak, "compiled_leak"] = tmp.loc[zeroleak, "nonzero_mean"]
         scores.append(np.sqrt(mean_squared_error(y, np.log1p(tmp["compiled_leak"]).fillna(14.49))))
@@ -144,7 +143,6 @@ if __name__ == '__main__':
     test["target"] = train["target"].mean()
 
     cols = ['f190486d6', '58e2e02e6', 'eeb9cd3aa', '9fd594eec', '6eef030c1', '15ace8c9f', 'fb0f5dbfe', '58e056e12', '20aa07010', '024c577b9', 'd6bb78916', 'b43a7cfd5', '58232a6fb', '1702b5bf0', '324921c7b', '62e59a501', '2ec5b290f', '241f0f867', 'fb49e4212', '66ace2992', 'f74e8f13d', '5c6487af1', '963a49cdc', '26fc93eb7', '1931ccfdd', '703885424', '70feb1494', '491b9ee45', '23310aa6f', 'e176a204a', '6619d81fc', '1db387535', 'fc99f9426', '91f701ba2', '0572565c2', '190db8488', 'adb64ff71', 'c47340d97', 'c5a231d81', '0ff32eb98']
-
     train_leak, result = compiled_leak_result()
 
     result = pd.DataFrame.from_dict(result, orient='columns')
