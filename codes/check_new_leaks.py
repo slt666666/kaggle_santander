56af3204c0e16cd939fb47cf91156b961366e246
堺俊之 <toshi6661024@gmail.com>
2018-08-16 22:37:27 +0900

    delete test print

--- codes/check_new_leaks.py
+++ codes/check_new_leaks.py
@@ -28,7 +28,6 @@ def check_base_list(i):
         base = pd.DataFrame([base])
         base = base.apply(tuple, axis=1).to_frame().rename(columns={0: 'key'})
         for j in range(26-k):
-            print("base:{0}..{1}_{2}, rest:{3}_{4}".format(i, 26-k, 41-k, j, j+15))
             # normal
             comp_base = rest_set.iloc[:, j:j+15].apply(tuple, axis=1).to_frame().rename(columns={0: 'key'})
             if pd.merge(comp_base, base, on='key').shape[0] > 0:
