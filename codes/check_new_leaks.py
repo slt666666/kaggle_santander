2914ce1dc60805e839175beea709a9847429f587
堺俊之 <toshi6661024@gmail.com>
2018-08-16 19:33:54 +0900

    fix path

--- codes/check_new_leaks.py
+++ codes/check_new_leaks.py
@@ -7,8 +7,8 @@ from sklearn import linear_model
 import sklearn
 %matplotlib inline
 
-leak_data = pd.read_csv('./input/test_leak.csv')
-test_data = pd.read_csv('./input/test.csv')
+leak_data = pd.read_csv('../input/test_leak.csv')
+test_data = pd.read_csv('../input/test.csv')
 
 cols = ['f190486d6', '58e2e02e6', 'eeb9cd3aa', '9fd594eec', '6eef030c1', '15ace8c9f', 'fb0f5dbfe', '58e056e12', '20aa07010', '024c577b9', 'd6bb78916', 'b43a7cfd5', '58232a6fb', '1702b5bf0', '324921c7b', '62e59a501', '2ec5b290f', '241f0f867', 'fb49e4212', '66ace2992', 'f74e8f13d', '5c6487af1', '963a49cdc', '26fc93eb7', '1931ccfdd', '703885424', '70feb1494', '491b9ee45', '23310aa6f', 'e176a204a', '6619d81fc', '1db387535', 'fc99f9426', '91f701ba2', '0572565c2', '190db8488', 'adb64ff71', 'c47340d97', 'c5a231d81', '0ff32eb98']
 # print(len(cols))
