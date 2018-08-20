147081fc57736678e3372f782e8b63e92c74fc96
堺俊之 <toshi6661024@gmail.com>
2018-08-20 15:35:27 +0900

    delete test col

--- codes/jiazhen.py
+++ codes/jiazhen.py
@@ -25,11 +25,11 @@ train = pd.read_csv("../input/train.csv")
 transact_cols = [f for f in train.columns if f not in ["ID", "target"]]
 y = np.log1p(train["target"]).values
 
-test["target"] = train["target"].mean()
+# test["target"] = train["target"].mean()
 
-all_df = pd.concat([train, test]).reset_index(drop=True)
-all_df.columns = all_df.columns.astype(str)
-print(all_df.shape)
+# all_df = pd.concat([train, test]).reset_index(drop=True)
+# all_df.columns = all_df.columns.astype(str)
+# print(all_df.shape)
 
 cols = ['f190486d6', '58e2e02e6', 'eeb9cd3aa', '9fd594eec', '6eef030c1', '15ace8c9f', 'fb0f5dbfe', '58e056e12', '20aa07010', '024c577b9', 'd6bb78916', 'b43a7cfd5', '58232a6fb', '1702b5bf0', '324921c7b', '62e59a501', '2ec5b290f', '241f0f867', 'fb49e4212', '66ace2992', 'f74e8f13d', '5c6487af1', '963a49cdc', '26fc93eb7', '1931ccfdd', '703885424', '70feb1494', '491b9ee45', '23310aa6f', 'e176a204a', '6619d81fc', '1db387535', 'fc99f9426', '91f701ba2', '0572565c2', '190db8488', 'adb64ff71', 'c47340d97', 'c5a231d81', '0ff32eb98']
 
